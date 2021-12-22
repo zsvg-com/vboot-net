6ca6067cf05e70b9e1b988e7a3b1412efe46bb24
zsvg <8680782@qq.com>
2021-12-22 19:59:08 +0800

    增加sqlserver匹配

--- Vboot.Core/Startup.cs
+++ Vboot.Core/Startup.cs
@@ -16,7 +16,7 @@ namespace Vboot.Core
         {
             services.AddSqlSugar(new ConnectionConfig
             {
-                ConnectionString = "Data Source=localhost;Database=vboot22;User ID=root;Password=123456;pooling=true;port=3306;sslmode=none;CharSet=utf8;",//连接符字串
+                ConnectionString = "Data Source=localhost;Database=vboot-net;User ID=root;Password=123456;pooling=true;port=3306;sslmode=none;CharSet=utf8;",//连接符字串
                 DbType = DbType.MySql,
                 //ConnectionString = "Data Source=localhost/orcl;User ID=vboot;Password=123456;",//连接符字串
                 //DbType = DbType.Oracle,
